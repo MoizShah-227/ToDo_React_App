707d9d47c5e6b11cfe980efca12cf419673cb704
MoizShah-227 <103181576+MoizShah-227@users.noreply.github.com>
2023-07-18 02:39:04 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Todowrapper from './components/TodoWrapper';
+
+function App() {
+  return (
+    <>
+    <Todowrapper/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+export const Todo = ({task, deleteTodo, editTodo, toggleComplete}) => {
+ 
+  return (
+    <div className="Todo">
+        <p className={`${task.completed ? 'completed' : ""}`} onClick={() => toggleComplete(task.id)}>{task.task}</p>
+        <div>
+        <button onClick={() => editTodo(task.id)} >Edit</button>
+        <button onClick={() => deleteTodo(task.id)} >Delete</button>
+        </div>
+    </div>
+  )
+}
